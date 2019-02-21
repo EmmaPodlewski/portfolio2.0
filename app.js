093a7adcc52d7c5c522e096b341caf9587576786
Emma Podlewski <emmanuelle.podlewski@freshbooks.com>
2019-02-21 13:08:01 -0800

    Add dymanic features

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//Burger Menu
+
 $(document).ready(function () {
 
   // Check for click events on the navbar burger icon
@@ -9,6 +11,52 @@ $(document).ready(function () {
 
   });
 });
+
+
+// Nav Position
+
+$(document).ready(function () {
+  var isMobile;
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    isMobile = true;
+  };
+  var navPos = $('nav').position().top;
+  var lastPos = 0;
+  var lockTimer
+
+  $(window).on('scroll', function () {
+
+    var pos = $(window).scrollTop();
+    var pos2 = pos + 50;
+    var scrollBottom = pos + $(window).height();
+
+    if (!isMobile) {
+      if (pos >= navPos + $('nav').height() && lastPos < pos) {
+        $('nav').addClass('sticky');
+      }
+      if (pos < navPos && lastPos > pos) {
+        $('nav').removeClass('sticky');
+      }
+      lastPos = pos;
+    }
+
+    if (pos2 > $('#home').offset().top) { highlightLink('home'); }
+    if (pos2 > $('#expertise').offset().top) { highlightLink('expertise'); }
+    if (pos2 > $('#aboutus').offset().top) { highlightLink('aboutus'); }
+    if (pos2 > $('#portfolio').offset().top) { highlightLink('portfolio'); }
+    if (pos2 > $('#contact').offset().top ||
+      pos + $(window).height() === $(document).height()) {
+      highlightLink('Contact');
+    }
+  });
+});
+
+function highlightLink(anchor) {
+  $('navbar-item .is-active').removeClass('is-active');
+  $("a").find('[navbar-item="' + anchor + '"]').addClass('is-active');
+}
+
+// Header Type Effect
 
 function typeEffect(element, speed) {
   var text = $(element).text();
@@ -34,3 +82,31 @@ $(document).ready(function () {
     typeEffect($('.subtitle-header'), speed);
   }, delay);
 });
+
+
+// Portfolio tab selection
+
+function showSelected(event, skill) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("is-active ", "");
+    tablinks[i].className = tablinks[i].className.replace(" is-active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  skills = document.getElementsByClassName(skill);
+  for(i=0; i < skills.length; i++) {
+    skills[i].style.display = "block";
+  }
+  event.currentTarget.className += " is-active";
+}
